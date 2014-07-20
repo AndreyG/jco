617c85d053937aba259081801a5cb3d792a3e54b
Andrey Davydov <andrey.a.davydov@gmail.com>
2014-07-21 01:20:10 +0400

    string serialization: process special symbols

--- src/printer_base.cpp
+++ src/printer_base.cpp
@@ -6,7 +6,45 @@ namespace jco
     {
         void PrinterBase::print(boost::string_ref str)
         {
-            backend_ << "\"" << str << "\"";
+            backend_ << "\"";
+            for (char c : str)
+            {
+                switch (c)
+                {
+                case '\"':
+                    backend_ << "\\\"";
+                    break;
+                case '\\':
+                    backend_ << "\\\\";
+                    break;
+                case '\b':
+                    backend_ << "\\b";
+                    break;
+                case '\n':
+                    backend_ << "\\n";
+                    break;
+                case '\r':
+                    backend_ << "\\r";
+                    break;
+                case '\t':
+                    backend_ << "\\t";
+                    break;
+                default:
+                    if (c < 32)
+                    {
+                        int i = c;
+                        backend_ << "\\u00" << ((i & 0xF0) >> 4);
+                        i &= 0xF;
+                        if (i < 10)
+                            backend_ << i;
+                        else
+                            backend_ << char('A' + (i - 10));
+                    }
+                    else
+                        backend_ << c;
+                }
+            }
+            backend_ << "\"";
         }
 
         void PrinterBase::print(bool f)
